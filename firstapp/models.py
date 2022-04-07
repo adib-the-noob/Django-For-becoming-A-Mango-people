d2e3f3580f84544818f154f3cafee2dc1d9c357f
Mohammed Adib <mdadib550@gmail.com>
2022-04-08 01:28:47 +0600

    Model Manager

--- firstapp/models.py
+++ firstapp/models.py
@@ -5,3 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=11)
     content = models.TextField()
+    
+    #Model Manager
+    def __str__(self):
+        return self.name

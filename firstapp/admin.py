76268d4a78956bec99fb8e19826b99294ae02f81
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2022-04-16 01:02:21 +0600

    Model Fields

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact,Post
 # Register your models here.
 
 #Registartion for Saving data to Database...
 admin.site.register(Contact)
+admin.site.register(Post)
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,4 +1,6 @@
+from venv import create
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Contact(models.Model):
@@ -9,3 +11,17 @@ class Contact(models.Model):
     #Model Manager
     def __str__(self):
         return self.name
+class Post(models.Model):
+    CATEGORY=(
+        ('Teacher', 'Teacher'),
+        ('Student', 'Student'),
+    )
+    id=models.AutoField(primary_key=True)
+    title=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=100,default=title)
+    email=models.EmailField()
+    salary=models.FloatField()
+    details=models.TextField()
+    available=models.BooleanField()
+    category=models.CharField(max_length=100,choices=CATEGORY)
+    created_at=models.DateTimeField(default=now)

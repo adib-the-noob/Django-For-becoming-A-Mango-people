09937856c532a1f25964303aef271305a06a88ee
TheNoob <mdadib550@gmail.com>
2022-04-20 20:08:03 +0600

    Registering Images

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import contact
+from .views import contact,PostView
 
 urlpatterns = [
     path('contact/', contact, name = 'contact'),
+     path('post/', PostView, name = 'posts'),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+
+from firstapp.models import Post
 # Create your views here.
 
 from .forms import ContactForm
-
-
 
 def contact(request):
     if request.method == "POST":
@@ -14,3 +14,7 @@ def contact(request):
         form=ContactForm()
     
     return render(request,"contact.html",{'form':form})
+
+def PostView(request):
+    post = Post.objects.all()
+    return render(request,'firstapp/postview.html',{'post': post})
